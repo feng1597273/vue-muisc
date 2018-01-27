c255eb7b3b2515fd40d7e036800cafb75459ecd5
feng <475829120@qq.com>
2018-01-27 17:30:54 +0800

    初始化项目3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <mhead></mhead>
+    <tab></tab>
     <router-view/>
   </div>
 </template>
 
 <script>
   import mhead from "@/base/header"
-export default {
+  import tab from '@/base/tab'
+  export default {
   name: 'App',
   components:{
-    mhead
+    mhead,
+    tab
   }
 }
 </script>
--- src/base/tab/index.vue
+++ src/base/tab/index.vue
@@ -1,5 +1,18 @@
 <template>
-
+  <div class="tab">
+    <router-link tag="div" class="tab-item" to="/recommend">
+      <span class="tab-link">推荐</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/singer">
+      <span class="tab-link">歌手</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/rank">
+      <span class="tab-link">排行</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/search">
+      <span class="tab-link">搜索</span>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -8,6 +21,13 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+.tab
+  display flex
+  height 44px
+  line-height 44px
+  font-size $font-size-medium
+  .tab-item
+    flex:1
+    text-align center
 </style>
